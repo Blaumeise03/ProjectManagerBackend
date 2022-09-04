bd34af86c5e371dfcce35e9c3eb060ae1ca0f9da
Blaumeise03 <bluegame61@gmail.com>
2022-09-04 16:26:37 +0200

    Improved logging.

--- src/main/java/de/blaumeise03/projectmanager/LoggingInterceptor.java
+++ src/main/java/de/blaumeise03/projectmanager/LoggingInterceptor.java
@@ -6,15 +6,35 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 public class LoggingInterceptor implements HandlerInterceptor {
     Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("Processed request from " + request.getRemoteAddr() + " to: " + request.getRequestURI());
+        Principal userPrincipal = request.getUserPrincipal();
+        HttpSession session = request.getSession();
+        String username = "N/A";
+        if(userPrincipal != null) username = userPrincipal.getName();
+        String sessionID = "N/A";
+        if (session != null) {
+            sessionID = session.getId();
+            int i = Math.max(sessionID.length() - 8, 0);
+            sessionID = sessionID.substring(0, i) + "********";
+        }
+        logger.info(
+                "Processed request from user '{}' (IP Hash '{}', session '{}') to: '{}'",
+                username,
+                request.getRemoteAddr().hashCode(),
+                sessionID,
+                request.getRequestURI()
+        );
+        logger.debug("IP for Hash {} is " + request.getRemoteAddr(), request.getRemoteAddr().hashCode());
         logger.debug("Request cookies: " + request.getHeader("cookie"));
-        logger.debug("Response Set-Cookie: " + response.getHeaders("set-cookie"));
+        if (!response.getHeaders("set-cookie").isEmpty())
+            logger.debug("Response Set-Cookie: " + response.getHeaders("set-cookie"));
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
